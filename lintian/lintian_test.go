ebb4ffbdc0f36f0aec9b3c477cea72765a2c06a8
Nicolas Peugnet <nicolas@club1.fr>
2024-08-22 21:23:50 +0200

    Add basic test for lintian package

---
+++ lintian/lintian_test.go
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2024 Nicolas Peugnet <nicolas@club1.fr>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package lintian_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/n-peugnet/lintian-ssg/lintian"
+)
+
+const baseURL = "https://salsa.debian.org/lintian/lintian/-/blob//tags/"
+
+func TestSource(t *testing.T) {
+	cases := []struct {
+		tag      lintian.Tag
+		expected string
+	}{
+		{ // Basic case
+			lintian.Tag{Name: "test-tag", NameSpaced: false},
+			"t/test-tag.tag",
+		},
+		{ // namespaced case
+			lintian.Tag{Name: "teams/js/test-tag", NameSpaced: true},
+			"teams/js/test-tag.tag",
+		},
+	}
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("%d %s", i, c.expected), func(t *testing.T) {
+		})
+		expected := baseURL + c.expected
+		actual := c.tag.Source()
+		if actual != expected {
+			t.Fatalf("\nexpected: %q\nactual  : %q", expected, actual)
+		}
+	}
+}
